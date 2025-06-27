8f5949920e7748b69140f81159176bf89a3a4c68
Taykazuhiro <taykazuhiro@gmail.com>
2025-06-27 20:57:17 -0300

    post and get de medicos e pacientes ok. Migraation ok.

---
+++ api/src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,14 @@
+package med.voll.api.medico;
+
+public enum Especialidade {
+    ORTOPEDIA ("ortopedia", "ortopedista"),
+    CARDIOLOGIA ("cardiologia", "cardiologista"),
+    GINECOLOGIA ("ginecologia", "ginecologista"),
+    DERMATOLOGIA ("dermatologia", "dermatologista");
+
+    Especialidade(String especialidadeConvertida){
+    }
+
+    Especialidade(String especialidadeConvertida, String especialidadeNovoFormato) {
+    }
+}
