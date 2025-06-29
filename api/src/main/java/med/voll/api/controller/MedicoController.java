4ace296ade453ecad3490ff0ec2e6e4f01b68848
Taykazuhiro <taykazuhiro@gmail.com>
2025-06-29 15:58:56 -0300

    crud completo

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,7 +26,31 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        //pego os dados do médico que já existem no meu banco de dados a partir do ID
+        var medico = repository.getReferenceById(dados.id());
+        //uso o metodo atualizarInformacoes para fazer o update no banco
+        medico.atualizarInformacoes(dados);
+        //não precisamos usar o metodo save do repository porque há a anotação @Transactional que entende que há uma transação de atualização
+    }
+//    COMO EXCLUIR REAL OFICIAL UM REGISTRO DO BANCO
+//    @DeleteMapping("/{id}")
+//    @Transactional
+//    public void excluir(@PathVariable Long id){
+//        repository.deleteById(id);
+//    }
+
+    // COMO FAZER EXCLUSAO LÓGICA
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.inativar();
     }
 
 }
